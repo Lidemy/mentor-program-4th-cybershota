bfef2bf87c442166a4e43117c7913dfe3c01b80a
zangwang <info@zangwang.tw>
2020-07-10 22:13:41 +0800

    重新整理作業專案

--- homeworks/week3/jest/hw4.js
+++
@@ -1,8 +0,0 @@
-function solve(arr) {
-  // 遇到物件傳參考特性，要小心
-  const strArr = arr[0].split('');
-  const reverseStr = strArr.reverse().join('');
-  console.log(arr[0] === reverseStr ? 'True' : 'False');
-}
-
-module.exports = solve;
