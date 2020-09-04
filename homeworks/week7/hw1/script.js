dd9cadad45993411e2dd35f91413c1c0800da887
zangwang <info@zangwang.tw>
2020-09-05 01:49:13 +0800

    新增字數統計，PDF 列印，完成 hw1

--- homeworks/week7/hw1/script.js
+++ homeworks/week7/hw1/script.js
@@ -1,13 +1,15 @@
 const form = document.querySelector('form');
-const modalResult = document.querySelectorAll('.modal-body ul li');
+const modalResult = document.querySelectorAll('.modal-body li');
 const phoneInput = document.querySelector('#cell-phone');
+const howknowInput = document.querySelector('#answer');
+const adviceInput = document.querySelector('#advice');
 const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal-body span');
+const printBtn = document.querySelector('#print');
 
 let elArry = [];
 let validArry = [];
-let data = [];
-
-console.log(modalResult);
+let data = {};
 
 function addWarning(element) {
   elArry.push(element);
@@ -19,11 +21,11 @@ function removeWarning(element) {
   form.childNodes[element].classList.remove('block-invalid', 'warning');
 }
 
-function insertModalResult(content) {
-  data.push(content);
-  if (data.length === 6) {
+function insertModalResult(key, value) {
+  data[key] = value;
+  if (Object.keys(data).length === 6) {
     for (let i = 0; i <= 5; i += 1) {
-      modalResult[i].childNodes[1].textContent = (data[i]);
+      modalResult[i].childNodes[1].textContent = Object.values(data)[i];
     }
   }
 }
@@ -38,7 +40,7 @@ function formValuate() {
   if (form.name.value.match(/^[^\s].{0,18}[^\s]$/g)) {
     removeWarning(3);
     validArry.push(true);
-    insertModalResult(form.name.value);
+    insertModalResult('name', form.name.value);
   } else {
     validArry.push(false);
     addWarning(3);
@@ -51,7 +53,7 @@ function formValuate() {
   ) {
     removeWarning(5);
     validArry.push(true);
-    insertModalResult(form.email.value);
+    insertModalResult('email', form.email.value);
   } else {
     validArry.push(false);
     addWarning(5);
@@ -60,7 +62,7 @@ function formValuate() {
   if (form.phone.value.match(/\+(886) ([\d]{3})-([\d]{3})-([\d]{3})/g)) {
     removeWarning(7);
     validArry.push(true);
-    insertModalResult(form.phone.value);
+    insertModalResult('phone', form.phone.value);
   } else {
     validArry.push(false);
     addWarning(7);
@@ -69,7 +71,7 @@ function formValuate() {
   if (form.category.value) {
     removeWarning(9);
     validArry.push(true);
-    insertModalResult(form.category.value);
+    insertModalResult('category', form.category.value);
   } else {
     validArry.push(false);
     addWarning(9);
@@ -79,7 +81,7 @@ function formValuate() {
     removeWarning(11);
     validArry.push(true);
     const output = form.howknow.value.trim();
-    insertModalResult(output);
+    insertModalResult('howknow', output);
   } else {
     validArry.push(false);
     addWarning(11);
@@ -88,7 +90,7 @@ function formValuate() {
   if (form.advice.value.match(/(.{1,300})/g)) {
     validArry.push(true);
     const output = form.advice.value.trim();
-    insertModalResult(output);
+    insertModalResult('advice', output);
   } else {
     validArry.push(true);
     insertModalResult('無');
@@ -104,11 +106,17 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   elArry = [];
   validArry = [];
-  data = [];
+  data = {};
+  modal.style.display = 'flex';
   formValuate();
   if (validArry.every(el => el === true)) {
     modal.style.display = 'flex';
   }
+});
+
+// 點擊叉叉關閉 Modal
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
 });
 
 // 點擊 Modal 灰色區域關閉 Modal
@@ -152,3 +160,45 @@ phoneInput.addEventListener('keyup', (event) => {
 });
 
 // 簡答字數統計提醒
+function countText(element, i, wordLimit) {
+  element.addEventListener('focus', () => {
+    form.childNodes[i].classList.add('text-count');
+  });
+  element.addEventListener('input', () => {
+    form.childNodes[i].dataset.count = `${element.value.length} / ${wordLimit}`;
+  });
+  element.addEventListener('focusout', () => {
+    form.childNodes[i].classList.remove('text-count');
+  });
+}
+
+countText(howknowInput, 11, 100);
+countText(adviceInput, 13, 300);
+
+// PDF 列印
+// 只有 client 端遇到好多依賴問題，我就直接硬畫ㄉ了...
+// 也是體會到要印出來是可以多麼ㄉ困難...
+function printUserData(e) {
+  e.preventDefault();
+  const { jsPDF } = window.jspdf;
+  // eslint-disable-next-line new-cap
+  const doc = new jsPDF({ unit: 'mm' });
+  doc.setFillColor(250, 211, 18);
+  doc.rect(5, 5, 200, 20, 'F');
+  doc.setFillColor(0, 0, 0);
+  doc.setFontSize(25);
+  doc.addFont('./src/TaipeiSansTCBeta-Regular.ttf', 'TaipeiSansTCBeta-Regular', 'normal');
+  doc.setFont('TaipeiSansTCBeta-Regular', 'normal');
+  doc.text('新拖延運動報名成功', 65, 18);
+  doc.setFontSize(15);
+  doc.text(`暱稱 | ${data.name}`, 30, 40);
+  doc.text(`電子信箱 | ${data.email}`, 30, 50);
+  doc.text(`手機號碼 | ${data.phone}`, 30, 60);
+  doc.text(`報名類型 | ${data.category}`, 30, 70);
+  doc.text(`怎麼知道活動 | ${data.howknow}`, 30, 80, { maxWidth: '150' });
+  doc.text(`其他 | ${data.advice}`, 30, 110, { maxWidth: '150' });
+  doc.save();
+}
+
+
+printBtn.addEventListener('click', printUserData);
